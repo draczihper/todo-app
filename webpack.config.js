a910a2b8f13c7077765ecab9d0610e80557ddf03
Drac Zihper <draczihper@gmail.com>
2024-08-05 18:14:08 +0300

    Add to config and add pkgs

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
-onst path = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      scriptLoading: 'defer',
+      inject: 'body',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
