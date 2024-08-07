7b7e13462d7108f79bca4c027af0a46b1e31a715
Drac Zihper <draczihper@gmail.com>
2024-08-08 00:11:06 +0300

    Add editTodo, hideBtn, deleteAll methods

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './styles.css';
 const form = document.querySelector('[data-form]');
 const lists = document.querySelector('[data-lists]');
 const input = document.querySelector('[data-input]');
+const deleteAllButton = document.querySelector('.delete-all-btn');
 
 // Store todos to localstorage
 class Storage {
@@ -32,6 +33,7 @@ form.addEventListener('submit', (e) => {
     DOM.removeTodo();
     // Add todo to storage
     Storage.addToStorage(todoArr);
+    DOM.hideBtn();
 
 });
 
@@ -49,8 +51,11 @@ class DOM {
         let displayTodo = todoArr.map(item => {
             return `
             <div class="todo">
-        <p>${item.todo}</p>
-          <span class="remove" data-id=${item.id}>🗑️</span>
+        <p class="todo-text">${item.todo}</p>
+        <div class="icon">
+        <span class="remove" data-id=${item.id}>🗑️</span>
+        <span class="edit" data-id=${item.id}>✏️</span>
+        </div>
       </div>
             `
         });
@@ -64,11 +69,12 @@ class DOM {
     static removeTodo() {
         lists.addEventListener('click', (e) => {
             if(e.target.classList.contains('remove')) {
-                e.target.parentElement.remove();
+                e.target.parentElement.parentElement.remove();
+                let btnId = e.target.dataset.id;
+                //Remove todo from the array
+                DOM.removeTodoFromArray(btnId);
+                DOM.hideBtn();
             }
-            let btnId = e.target.dataset.id;
-            //Remove todo from the array
-            DOM.removeTodoFromArray(btnId);
         });
     }
 
@@ -76,9 +82,59 @@ class DOM {
         todoArr = todoArr.filter((item) => item.id !== +id)
         Storage.addToStorage(todoArr)
     }
+
+    static editTodo() {
+        let iconChange = true;
+        lists.addEventListener('click', (e) => {
+            if(e.target.classList.contains('edit')) {
+                let p = e.target.parentElement.parentElement.firstElementChild;
+                const btnId = e.target.dataset.id;
+                if (iconChange) {
+                    p.setAttribute("contenteditable", "true");
+                    p.focus();
+                    e.target.textContent = "Save";
+                    p.style.color = "blue";
+                } else {
+                    e.target.textContent = "✏️";
+                    p.style.color = "black";
+                    p.removeAttribute("contenteditable");
+                    const newTodoArr = todoArr.findIndex((item) => item.id === +btnId);
+                    todoArr[newTodoArr].todo = p.textContent;
+                    Storage.addToStorage(todoArr);
+                }
+            }
+            iconChange = !iconChange
+        });
+    }
+
+    static deleteAll() {
+        deleteAllButton.addEventListener('click', () => {
+            todoArr.length = 0;
+            localStorage.clear();
+            DOM.displayTodo();
+            DOM.hideBtn();        
+        });
+    }
+
+    static hideBtn() {
+        if(todoArr.length <= 0) {
+            deleteAllButton.style.display = "none";
+        } else {
+            deleteAllButton.style.display = "flex";
+        }
+    }
+    
 }
 
+// Load saved todos in localStorage when app is loaded
 window.addEventListener('DOMContentLoaded', () => {
     DOM.displayTodo();
-    DOM.removeTodo()
+    // Remove from the DOM
+    DOM.removeTodo();
+
+    DOM.editTodo();
+
+    DOM.deleteAll();
+
+    DOM.hideBtn();
 });
